7ab59b03b53bb7958827ff527a78d38c8c48bb20
Daniel Liang <daniel65@live.ca>
2021-12-14 17:12:52 -0500

    Reduced edge thickness to improve legibility and removed unneccesary code from prettified version

--- solutions/python-igraph/visualize_cliques/visualize_cliques.py
+++ solutions/python-igraph/visualize_cliques/visualize_cliques.py
@@ -1,10 +1,12 @@
 import igraph as ig
 import matplotlib.pyplot as plt
 
-g = ig.Graph.Famous("Zachary")
+g = ig.Graph.Famous('Zachary')
+g.es['width'] = 0.5
 cliques = g.cliques(4, 4)
 fig, ax = plt.subplots(3, 4)
 for i in range(len(cliques)):
     ig.plot(ig.VertexCover(g, [cliques[i]]), mark_groups=True, target=ax[i // 4, i % 4])
+plt.axis('off')
 plt.show()
 
--- solutions/python-igraph/visualize_cliques/visualize_cliques_with_edges.py
+++ solutions/python-igraph/visualize_cliques/visualize_cliques_with_edges.py
@@ -1,44 +1,27 @@
 import igraph as ig
 import matplotlib.pyplot as plt
 
-# Set up the base Zachary Karate Club graph
-g = ig.Graph.Famous("Zachary")
-g.vs["label"] = [i + 1 for i in range(g.vcount())]
-base_visual_style = {
-    "vertex_size": 0,
-    "vertex_color": "white",
-    "edge_width": 0.3
-}
+g = ig.Graph.Famous('Zachary')
+g.vs['label'] = [i + 1 for i in range(g.vcount())]
+g.vs['size'] = 0
+g.es['width'] = 0.3
+g.vs['color'] = 'yellow'
 
-# Generate a sorted list of all eleven 4-cliques
+fig, ax = plt.subplots(3, 4)
 cliques = g.cliques(4, 4)
-
-# Generate 11 subplots, each highlighting one 4-clique and its edges
-clique_visual_styles = []
 for i in range(len(cliques)):
-    clique_visual_styles.append(dict(base_visual_style))
-    vertex_colors = ["white"] * g.vcount()
-    edge_colors = ["black"] * g.ecount()
-    vertex_sizes = [0] * g.vcount()
-    edge_widths = [0.3] * g.ecount()
-
-    for j in cliques[i]:
-        vertex_colors[j] = "yellow"
-        vertex_sizes[j] = 0.6
-        
+    g.vs[cliques[i]]['size'] = 0.5
     for j in range(len(cliques[0]) - 1):
         for k in range(j + 1, len(cliques[0])):
-            edge_colors[g.get_eid(cliques[i][j], cliques[i][k])] = "red"
-            edge_widths[g.get_eid(cliques[i][j], cliques[i][k])] = 1
-
-    clique_visual_styles[i]["vertex_color"] = list(vertex_colors)
-    clique_visual_styles[i]["vertex_size"] = list(vertex_sizes)
-    clique_visual_styles[i]["edge_color"] = list(edge_colors)
-    clique_visual_styles[i]["edge_width"] = list(edge_widths)
-
-# Create 3 rows of 4 subplots
-fig, ax = plt.subplots(3, 4)
-for i in range(len(clique_visual_styles)):
-    ig.plot(ig.VertexCover(g, [cliques[i]]), mark_groups=True, target=ax[i // 4, i % 4], **clique_visual_styles[i])
+            g.es[g.get_eid(cliques[i][j], cliques[i][k])]['color'] = 'red'
+            g.es[g.get_eid(cliques[i][j], cliques[i][k])]['width'] = 1
+            
+    ig.plot(ig.VertexCover(g, [cliques[i]]), mark_groups=True, target=ax[i // 4, i % 4])
+    
+    g.vs['size'] = 0
+    g.es['color'] = 'black'
+    g.es['width'] = 0.3
+    
+plt.axis('off')
 plt.show()
 
