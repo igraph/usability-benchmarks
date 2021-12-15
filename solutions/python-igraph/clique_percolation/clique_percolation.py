e57f36241edf09eee80ae0b402f17e51c1a681ad
Chenyan Wang <chenyan.wang@mail.utoronto.ca>
2021-12-15 15:12:25 -0500

    Add k-communities function and changed example to load Zachary's Karate Club graph

--- solutions/python-igraph/clique_percolation/clique_percolation.py
+++ solutions/python-igraph/clique_percolation/clique_percolation.py
@@ -1,6 +1,6 @@
 from igraph import Graph
-import itertools
 
+EXAMPLE_K = 3
 
 def k_cliques(g, k):
     """Returns a list of all cliques of size k in Graph g. Each clique is given as a set of vertex ids in g.
@@ -21,9 +21,73 @@ def k_cliques(g, k):
     """
     return g.cliques(k, k)
 
+def k_communities(g, k, min_common_vertices):
+    """
+    Given a graph and size k, return a list of communities found through clique
+    percolation with at least min_common_vertices shared.
+    A community is the connected component of cliques where two cliques overlap
+    in at least k-1 vertices.
+    Each community is a set of vertices in that community
+
+    Approach: generate a new clique graph where each k-clique is a vertex, and 
+    connect vertices if the cliques share min_common_vertices
+    """
+    if (min_common_vertices > k):
+        raise ValueError("Error: min_common_vertices=%d must be greater than k=%d" % (min_common_vertices, k))
+
+    # Each clique can be identified by it's index in 'cliques'
+    cliques = [set(x) for x in k_cliques(g, k)]
+    num_cliques = len(cliques)
+    # Graph storing each clique as a vertex
+    clique_graph = Graph()
+    clique_graph.add_vertices(num_cliques)
+    cg_edges = []
+    # List of communities as a list of list of clique indices
+    clique_communities = []
+    # List of communities as a list of list of set of vertices
+    communities = []
+
+    # Get connected cliques
+    for c1 in range(num_cliques):
+        for c2 in range(c1, num_cliques):
+            # If the two cliques share at least min_common_vertices,
+            # they are part of the same community
+            if c1 != c2 and len(cliques[c1].intersection(cliques[c2])) >= min_common_vertices:
+                cg_edges.append((c1, c2))
+    
+    # Add edges for clique graph
+    clique_graph.add_edges(cg_edges)
+
+    # Merge cliques accordingly into communities
+    to_merge = list(range(num_cliques))
+    while (len(to_merge) > 0):
+        # Get first unmerged vertex
+        idx = to_merge[0]
+        # Get all connected vertices
+        vertices = clique_graph.subcomponent(idx, mode="all")
+        # Remove all connected vertices
+        for v in vertices:
+            to_merge.remove(v)
+        clique_communities.append(vertices)
+
+    # Generate the list of clique communities as a list of set of vertices
+    for cc in clique_communities:
+        vertices = set()
+        for c in cc:
+            clique = cliques[c]
+            vertices.update(clique)
+        communities.append(vertices)
+
+    return communities
 
 if __name__ == "__main__":
-    g = Graph()
-    g.add_vertices(7)
-    g.add_edges([(0, 1), (1, 2), (1, 3), (1, 5), (2, 3), (2, 4), (2, 6), (3, 4), (3, 6), (4, 5), (4, 6)])
-    print(k_cliques(g, 3))
+    # Load in Zachary's karate club network
+    print("For the Zachary Karate Club graph\n")
+    g = Graph.Famous("Zachary")
+
+    # Calculate cliques and communities
+    print("Cliques of size k=%d" % EXAMPLE_K)
+    print(k_cliques(g, EXAMPLE_K))
+    print("")
+    print("Communities from cliques of size k=%d, with %d shared vertices" % (EXAMPLE_K, EXAMPLE_K-1))
+    print(k_communities(g, EXAMPLE_K, EXAMPLE_K-1))
