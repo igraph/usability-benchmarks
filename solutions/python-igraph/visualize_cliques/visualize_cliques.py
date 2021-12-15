8cfa1785f912d056ad1652d86bf90adefa2c9b40
Daniel Liang <daniel65@live.ca>
2021-12-15 12:57:03 -0500

    Use RainbowPalette to make VertexCover red, removed vertex labels and refactored the code

--- solutions/python-igraph/visualize_cliques/visualize_cliques.py
+++ solutions/python-igraph/visualize_cliques/visualize_cliques.py
@@ -2,11 +2,9 @@ import igraph as ig
 import matplotlib.pyplot as plt
 
 g = ig.Graph.Famous('Zachary')
-g.es['width'] = 0.5
 cliques = g.cliques(4, 4)
 fig, ax = plt.subplots(3, 4)
-for i in range(len(cliques)):
-    ig.plot(ig.VertexCover(g, [cliques[i]]), mark_groups=True, target=ax[i // 4, i % 4])
+for i, clique in enumerate(cliques):
+    ig.plot(ig.VertexCover(g, [clique]), mark_groups=True, palette=ig.RainbowPalette(), edge_width=0.5, target=ax[i // 4, i % 4])
 plt.axis('off')
 plt.show()
-
--- solutions/python-igraph/visualize_cliques/visualize_cliques_with_edges.py
+++ solutions/python-igraph/visualize_cliques/visualize_cliques_with_edges.py
@@ -2,26 +2,20 @@ import igraph as ig
 import matplotlib.pyplot as plt
 
 g = ig.Graph.Famous('Zachary')
-g.vs['label'] = [i + 1 for i in range(g.vcount())]
-g.vs['size'] = 0
-g.es['width'] = 0.3
-g.vs['color'] = 'yellow'
 
 fig, ax = plt.subplots(3, 4)
 cliques = g.cliques(4, 4)
-for i in range(len(cliques)):
-    g.vs[cliques[i]]['size'] = 0.5
-    for j in range(len(cliques[0]) - 1):
-        for k in range(j + 1, len(cliques[0])):
-            g.es[g.get_eid(cliques[i][j], cliques[i][k])]['color'] = 'red'
-            g.es[g.get_eid(cliques[i][j], cliques[i][k])]['width'] = 1
-            
-    ig.plot(ig.VertexCover(g, [cliques[i]]), mark_groups=True, target=ax[i // 4, i % 4])
-    
-    g.vs['size'] = 0
+for i, clique in enumerate(cliques):
+    g.vs['color'] = 'yellow'
     g.es['color'] = 'black'
     g.es['width'] = 0.3
     
+    g.vs[clique]['color'] = 'red'
+    clique_edges = g.es.select(_within=clique)
+    clique_edges['color'] = 'red'
+    clique_edges['width'] = 1
+            
+    ig.plot(ig.VertexCover(g, [clique]), mark_groups=True, palette=ig.RainbowPalette(), target=ax[i // 4, i % 4])
+  
 plt.axis('off')
 plt.show()
-
