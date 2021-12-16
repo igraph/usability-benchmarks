372e0789b4cf46b92a15ef6580f37c74d7d204b4
Yanchen Ma <yanchenm@gmail.com>
2021-12-16 18:16:18 -0500

    implement vertex and edge color by community

--- solutions/python-igraph/visualize_communities/visualize_communities_detailed.py
+++ solutions/python-igraph/visualize_communities/visualize_communities_detailed.py
@@ -5,27 +5,26 @@ from matplotlib.patches import Patch
 g = ig.Graph.Famous("Zachary")
 
 # Use edge betweenness to detect communities
+# and covert into a VertexClustering
 communities = g.community_edge_betweenness()
-
-# Convert the VertexDendrogram into a VertexCover for plotting
-communities = communities.as_clustering().as_cover()
+communities = communities.as_clustering()
 num_communities = len(communities)
 
-# Create a custom color legend
+# Color each vertex and edge based on its community membership
 palette = ig.RainbowPalette(n=num_communities)
+for i, community in enumerate(communities):
+    g.vs[community]["color"] = i
+    community_edges = g.es.select(_within=community)
+    community_edges["color"] = i
+
+# Create a custom color legend
 legend_elements = [
     Patch(facecolor=palette.get(i), edgecolor="k", label=i)
     for i in range(num_communities)
 ]
 
-# Visualize the communities simply with vertex cover coloring
+# Plot with only vertex and edge coloring
 fig, ax = plt.subplots()
-ig.plot(
-    communities,
-    mark_groups=True,
-    palette=ig.RainbowPalette(n=num_communities),
-    edge_width=0.5,
-    target=ax,
-)
+ig.plot(communities, palette=palette, edge_width=1, target=ax)
 ax.legend(handles=legend_elements)
 plt.show()
