3b5f0dc2b4a255a918e2596a75578617b88b3527
Chenyan Wang <chenyan.wang@mail.utoronto.ca>
2021-12-16 10:44:48 -0500

    Clean up code readability

--- solutions/python-igraph/clique_percolation/clique_percolation.py
+++ solutions/python-igraph/clique_percolation/clique_percolation.py
@@ -4,11 +4,6 @@ import matplotlib.pyplot as plt
 
 EXAMPLE_K = 3
 
-def k_cliques(g, k):
-    """Returns a list of all cliques of size k in Graph g. Each clique is given as a set of vertex ids in g.
-    """
-    return g.cliques(k, k)
-
 def k_communities(g, k, min_common_vertices):
     """
     Given a graph and size k, return a list of communities found through clique
@@ -23,12 +18,12 @@ def k_communities(g, k, min_common_vertices):
     if (min_common_vertices > k):
         raise ValueError("Error: min_common_vertices=%d must be greater than k=%d" % (min_common_vertices, k))
 
+    # Cliques of size k in graph g
+    k_cliques = g.cliques(k, k)
+
     # Each clique can be identified by it's index in 'cliques'
-    cliques = list(map(set, k_cliques(g, k)))
+    cliques = [set(clique) for clique in k_cliques]
     num_cliques = len(cliques)
-    # Graph storing each clique as a vertex
-    clique_graph = ig.Graph()
-    clique_graph.add_vertices(num_cliques)
     # List of communities as a list of list of set of vertices
     communities = []
 
@@ -37,8 +32,8 @@ def k_communities(g, k, min_common_vertices):
     # they are part of the same community
     cg_edges = [(c1, c2) for c1, c2 in itertools.combinations(range(num_cliques), 2) if len(cliques[c1].intersection(cliques[c2])) >= min_common_vertices]
 
-    # Add edges for clique graph
-    clique_graph.add_edges(cg_edges)
+    # Graph storing each clique as a vertex
+    clique_graph = ig.Graph(cg_edges)
 
     # Merge cliques accordingly into communities
     cg_clusters = clique_graph.clusters()
@@ -52,22 +47,18 @@ def k_communities(g, k, min_common_vertices):
 
     return communities
 
-if __name__ == "__main__":
-    # Load in Zachary's karate club network
-    print("For the Zachary Karate Club graph\n")
-    g = ig.Graph.Famous("Zachary")
+# Load in Zachary's karate club network
+print("For the Zachary Karate Club graph\n")
+g = ig.Graph.Famous("Zachary")
 
-    # Calculate cliques and communities
-    communities = k_communities(g, EXAMPLE_K, EXAMPLE_K-1)
-    print("Cliques of size k=%d" % EXAMPLE_K)
-    print(k_cliques(g, EXAMPLE_K))
-    print("")
-    print("Communities from cliques of size k=%d, with %d shared vertices" % (EXAMPLE_K, EXAMPLE_K-1))
-    print(communities)
-    
-    # plot the graph with a vertex cover for each community
-    fig, ax = plt.subplots()
-    ig.plot(ig.VertexCover(g, communities), mark_groups=True, palette=ig.RainbowPalette(n=3), edge_width=0.5, target=ax)
-    plt.axis('off')
-    plt.show()
-    
+# Calculate cliques and communities
+communities = k_communities(g, EXAMPLE_K, EXAMPLE_K-1)
+print("Communities from cliques of size k=%d, with %d shared vertices" % (EXAMPLE_K, EXAMPLE_K-1))
+print(communities)
+
+# Plot the graph with a vertex cover for each community
+fig, ax = plt.subplots()
+ig.plot(ig.VertexCover(g, communities), mark_groups=True, palette=ig.RainbowPalette(n=3), edge_width=0.5, target=ax)
+plt.axis('off')
+plt.show()
+
